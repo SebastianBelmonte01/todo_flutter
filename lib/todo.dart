8b79eb3fe18d3bd0e17a141239a467e808abfa41
Sebastian Francisco Belmonte Cerveró <sebastian.belmonte01@gmail.com>
2023-02-17 02:00:04 -0400

    Adding List View

--- lib/todo.dart
+++ lib/todo.dart
@@ -1,5 +1,34 @@
 import 'package:flutter/material.dart';
 
+
+class MyScaffold extends StatelessWidget {
+  const MyScaffold({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return
+        ListView(
+          children: [
+           _tile('CineArts at the Empire', '85 W Portal Ave', Icons.theaters),
+           _tile('CineArts at the Empire', '85 W Portal Ave', Icons.theaters),
+          ],
+        );
+  }
+  ListTile _tile(String title, String subtitle, IconData icon) {
+  return ListTile(
+    title: Text(title,
+        style: const TextStyle(
+          fontWeight: FontWeight.w500,
+          fontSize: 20,
+        )),
+    subtitle: Text(subtitle),
+    leading: Icon(
+      icon,
+      color: Colors.blue[500],
+    ),
+  );
+}
+}
 class MyTodo extends StatelessWidget {
   const MyTodo({super.key});
 
@@ -7,7 +36,8 @@ class MyTodo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("ToDo List", style: TextStyle(fontFamily: "Roboto", fontWeight: FontWeight.w900, fontSize: 30)), backgroundColor: Colors.blue[900],),
-
+      body: MyScaffold(),
+      
     );
   }
 }
